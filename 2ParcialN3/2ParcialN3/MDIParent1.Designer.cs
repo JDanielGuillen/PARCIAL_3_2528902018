d8f575235b9710b9b81183b523d6a092d9ae809a
JDanielGuillen <48265446+JDanielGuillen@users.noreply.github.com>
2019-04-23 20:02:50 -0600

    Add files via upload

---
+++ 2ParcialN3/2ParcialN3/MDIParent1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace _2ParcialN3
+{
+    partial class MDIParent1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.primerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.segundoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(270, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "MenuStrip";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.primerToolStripMenuItem,
+            this.segundoToolStripMenuItem});
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.abrirToolStripMenuItem.Text = "Menu";
+            // 
+            // primerToolStripMenuItem
+            // 
+            this.primerToolStripMenuItem.Name = "primerToolStripMenuItem";
+            this.primerToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.primerToolStripMenuItem.Text = "Primer";
+            this.primerToolStripMenuItem.Click += new System.EventHandler(this.primerToolStripMenuItem_Click);
+            // 
+            // segundoToolStripMenuItem
+            // 
+            this.segundoToolStripMenuItem.Name = "segundoToolStripMenuItem";
+            this.segundoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.segundoToolStripMenuItem.Text = "Segundo";
+            this.segundoToolStripMenuItem.Click += new System.EventHandler(this.segundoToolStripMenuItem_Click);
+            // 
+            // MDIParent1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 248);
+            this.Controls.Add(this.menuStrip);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MDIParent1";
+            this.Text = "MDIParent1";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem primerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem segundoToolStripMenuItem;
+    }
+}
+
+
+

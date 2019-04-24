c58bdac843925e90c4cb80d9e1b256bb17323cda
JDanielGuillen <48265446+JDanielGuillen@users.noreply.github.com>
2019-04-23 20:01:36 -0600

    Primera Ejercicio del parcial 3

---
+++ Parcial3/Parcial3/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "daniel" && textBox2.Text =="jdaniel100")
+            {
+                Form2 hijo = new Form2();
+                hijo.Show();
+                
+
+                   
+            }
+        }
+    }
+}
